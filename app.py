f4f12201f25d890fafb92b9998b511b1581dde8b
Dane Williams <dwilli36@nd.edu>
2020-12-18 18:16:50 -0800

    presigned url

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
+import json
+import logging
+import random
+import string
+import boto3
+from botocore.exceptions import ClientError
 from chalice import Chalice
 
+
 app = Chalice(app_name='imessage-viz')
+s3_client = boto3.client('s3', region_name='us-west-1')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+def get_random_id(length=8)->str:
+    '''Generates a random id for the file to be uploaded'''
+    chars = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
+
+@app.route('/', methods=['POST'], cors=True)
+def get_presigned_url():
+    '''returns the presigned url for uploading file to the s3 bucket'''
+    file_spec = app.current_request.json_body
+    name = get_random_id() + '.' + file_spec['name'].split('.')[1]
+
+    try: 
+        result = {'url': s3_client.generate_presigned_url(ClientMethod ='put_object',
+                                                          Params = {'Bucket': 'imessage-viz',
+                                                                    'Key': 'f/' + name,
+                                                                    'ContentType': file_spec['type']
+                                                                   },
+                                                          ExpiresIn = 30
+                                                         ),
+                  'name:': name
+                 }
+    except ClientError as e:
+        logging.error(e)
+        return {"error": "could not generate s3 presigned url"}
+
+    return result
 
 
 # The view function above will return {"hello": "world"}
