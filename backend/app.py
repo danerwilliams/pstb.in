d67718ad85e3aa38e5777015390da3c75a5a6e89
Dane Williams <dwilli36@nd.edu>
2020-12-20 20:42:36 -0800

    created new functions for url shortening

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+######################
+# Imports
+######################
 import json
 import logging
 import random
@@ -7,28 +10,78 @@ from botocore.exceptions import ClientError
 from chalice import Chalice
 
 
+########################
+# Globals
+########################
 app = Chalice(app_name='pstbin')
-s3_client = boto3.client('s3', region_name='us-west-1')
+s3 = boto3.client('s3')
 
 
-def get_random_id(length=8)->str:
+#########################
+# Utilities
+#########################
+def get_id_length(folder = '')->int:
+    '''calculates an id length recommendation based on bucket capacity'''
+    bucket = boto3.resource('s3').Bucket('www.pstb.in')
+    if folder:
+        capacity = sum(1 for _ in bucket.objects.filter(Prefix= folder + '/')) # counts number of objects in a specified folder
+    else:
+        capacity = sum(1 for _ in bucket.objects.filter(Delimiter='/')) # counts number of objects in root of bucket
+
+    recommended_length = 8
+    # C^R(n,r) = (n+r-1)!/r!(n-1)!  , where there are n=26 letters + 10 integers and r = length
+    max_capacities = [666, 8436, 82251, 658008, 4496388, 26978328]
+    for max_capacity, index in enumerate(max_capacities):
+        if capacity < max_capacity//2: #keep bucket under half of maximum capacity
+            recommended_length = index + 2
+
+    return recommended_length
+
+def get_random_id(length=2)->str:
     '''Generates a random id for the file to be uploaded'''
     chars = string.ascii_lowercase + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
 
+def format_target_url(source)->str:
+    '''cleans up input url aka github.com returns https://github.com and invalid;url.com returns None'''
+    
+
+
+########################
+# API Routes
+########################
+@app.route('/short', methods=['POST'], cors=True, content_types=['application/x-www-form-urlencoded'])
+def get_shortened_url():
+    '''returns shortened url for the desired '''
+    # post request data
+    body = app.current_request.raw_body.decode("utf-8")
+
+    # randomly generate new id until one is available
+    length = get_id_length() # url redirect objects are stored in top level folder of bucket
+    short = get_random_id(length)
+
+    with open('/tmp/totally_arbitrary_file', 'w') as _:
+        response = s3.upload_file('/tmp/test', 'www.pstb.in', short, ExtraArgs = {'WebsiteRedirectLocation': body})
+    return {'body': body}
 
 @app.route('/', methods=['POST'], cors=True)
-def get_presigned_url():
+def get_s3_presigned_url():
     '''returns the presigned url for uploading file to the s3 bucket'''
     body = app.current_request.json_body
-    name = get_random_id() + '.' + body['name'].split('.')[1]
+
+    length = get_id_length('f') # pasted files are stored in the f folder of bucket
+    name = get_random_id(length)
+
+    if '.' in body['name']: #give same extension as uploaded file if there is one
+        name += '.' + body['name'].split('.')[1]
+
     try: 
         result = {
-                  'url': s3_client.generate_presigned_url(
+                  'url': s3.generate_presigned_url(
                                                           ClientMethod = 'put_object',
                                                           Params       = {
-                                                                          'Bucket': 'pstbin',
-                                                                          'Key': name,
+                                                                          'Bucket': 'www.pstb.in',
+                                                                          'Key': 'f/' + name,
                                                                           'ContentType': body['type']
                                                                          },
                                                           ExpiresIn    = 3600
--- test.py
+++ test.py
@@ -3,17 +3,48 @@ import boto3
 import requests
 import json
 
+def format_target_url(source)->str:
+    '''cleans up input url aka github.com returns https://github.com and invalid;url.com returns None'''
+    
 def main():
-    s3 = boto3.client('s3')
-    print(s3.get_bucket_acl(Bucket='imessage-viz'))
+    print(format_target_url('github.com'))
+    print(format_target_url('')
+    # s3 = boto3.client('s3')
+    # bucket = boto3.resource('s3').Bucket('www.pstb.in')
+    # print(sum(1 for _ in bucket.objects.filter(Delimiter='/'))) # counts number of objects in root of bucket
+    # with open('/tmp/test', 'w') as _:
+    #     response = s3.upload_file('/tmp/test', 'www.pstb.in', 'newtest', ExtraArgs = {'WebsiteRedirectLocation': 'github.com'})
+    # print(response)
 
-    tructus = {'type': 'image/png', 'name': 'FisherBasementLogo.png'}
-    response = requests.post('https://68fwpup9dc.execute-api.us-west-1.amazonaws.com/api/', json=tructus)
-    guy = response.json()
+    # tructus = {'type': 'image/png', 'name': 'test.png'}
+    # response = requests.post('https://1vey8nkf2j.execute-api.us-west-1.amazonaws.com/api/', json=tructus)
+    # guy = response.json()
+    # print(guy)
 
-    headers = {'Content-Type': 'image/png'}
-    f = open('./FisherBasementLogo.png', 'rb')
-    result = requests.put(guy['body']['url'], headers=headers, data=f)
+    # headers = {'Content-Type': 'image/png', 'x-amz-website-redirect-location': 'https://github.com'}
+    # f = open('./test.png', 'rb')
+    # result = requests.put(guy['body']['url'], headers=headers, data=f)
+    # print(result.text)
+
+    # tructus = {'type': '', 'name': 'test'}
+    # response = requests.post('https://1vey8nkf2j.execute-api.us-west-1.amazonaws.com/api/', json=tructus)
+    # guy = response.json()
+    # print(guy)
+
+    # headers = {'x-amz-website-redirect-location': 'https://github.com'}
+    # f = open('./test', 'rb')
+    # result = requests.put(guy['body']['url'], headers=headers, data=f)
+    # print(result.text)
+
+    # tructus = {'type': 'image/png', 'name': 'test.png'}
+    # response = requests.post('https://1vey8nkf2j.execute-api.us-west-1.amazonaws.com/api/', json=tructus)
+    # guy = response.json()
+    # print(guy)
+
+    # headers = {'Content-Type': 'image/png'}
+    # f = open('./test.png', 'rb')
+    # result = requests.put(guy['body']['url'], headers=headers, data=f)
+    # print(result)
 
 
 if __name__ == '__main__':
