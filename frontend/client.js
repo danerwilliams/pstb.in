86cc2944cedfdf193e6143c705b83717aaa4777c
Dane Williams <dwilli36@nd.edu>
2020-12-21 22:33:23 -0800

    adds button handler

--- frontend/client.js
+++ frontend/client.js
@@ -2,15 +2,37 @@
 // Lambda API Endpoint
 const api = 'https://ez02ob0o22.execute-api.us-west-1.amazonaws.com/api/'
 
+/* Submit handler */
+function submitHandler() {
+    const url = document.getElementById('target_url').value;
+    const file = document.getElementById('target_file').value;
+    console.log(file);
+
+    if (!url && file)
+        getPresignedUrl(file);
+    else if (url && !file)
+        shortenUrl(url);
+    else
+        displayUrl('enter either a file or url');
+}
 
 /* Get shortened url */
-function shortenUrl() {
-    const target_url = document.getElementById('target_url').value;
+function shortenUrl(target_url) {
+    // const target_url = document.getElementById('target_url').value;
     fetch(api + 'shorten', {method: 'POST', mode: 'cors', body: target_url})
         .then(response => response.json())
         .then(response => displayUrl(response['body']['url']));
 }
 
+/* Get presigned url for file upload */
+function getPresignedUrl() {
+    
+}
+
+/* Upload file */
+function uploadFile() {
+
+}
 
 /* Displays shortened url */
 function displayUrl(url){
